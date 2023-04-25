83132df3af9ef323d66f8b7e70b73e00f90490a7
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-04-25 15:15:28 -0500

    completed exercises with multi-language greeting

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,66 +3,66 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-//a controller annotation tells spring boot that this class is a web controller
-// - in other words, it tells spring boot that there are methods in this class that are set up to handle http requests
-//the annotation is required for it to work
-
 @Controller
-@ResponseBody//returns a plain html text response
-@RequestMapping("hello")//this makes it clear that every single request mapping path should begin with /hello
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
-//controller handler method
-    //the first 3 methods are static (including the commented out one
-    //by default (because nothing else is specified) this method lives at the route path localhost:8080/
-//    @GetMapping //this method only returns a http get method
-//    @ResponseBody //returns a plain html text response
-//    public String hello(){
-//        return "Hello, Spring!";
+
+//    @GetMapping("goodbye")
+//    public String goodbye(){
+//        return "Goodbye, Spring!";
 //    }
 
-    //the method below handles request at localhost:8080/hello
-//    @GetMapping("hello") //specifies the path this handler lives at localhost:8080/hello
-//    @ResponseBody //returns a plain html text response
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
-// because of the Request mapping annotation at the top,
-    //this lives at /hello/goodbye
-    @GetMapping("goodbye") //this method only returns a http get method
-
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
-
-    //the handler below handles request of the form /hello?name=LaunchCode
-    //in other words, it takes a query parameter called name with a given value - LaunchCode - and that value should be used in the response
-// this is a dynamic handler - that means it accepts data
-    //the query parameter name and the method parameter name must match up
-// because of the Request mapping annotation at the top,
-    //this lives at /hello/hello
-    @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST}, value = "hello")
-
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
-    //handles requests of the form /hello/LaunchCode
-    //this means that the url includes the pathparameter
-    //name is the variable name - curly brackets & the @PathVariable denote this
-//    @GetMapping("hello/{name}")
+//    @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST}, value = "hello")
 //    @ResponseBody
-//    public String helloWithPathParam(@PathVariable String name){
-//        return name;
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
 //    }
-// because of the Request mapping annotation at the top,
-    //this lives at /hello/form
-    @GetMapping("form")
 
+
+    @RequestMapping(value="hello",method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language){
+        if (name == null){
+            name = "World";
+        }
+        return createMessage(name, language);
+    }
+
+    @RequestMapping(value="message",method = RequestMethod.POST)
+    @ResponseBody
+    public static String createMessage(@RequestParam String n, @RequestParam String l){
+        String greeting = "";
+        if(l.equals("english")) {
+            greeting = "Hello";
+        } else if(l.equals("french")) {
+            greeting = "Bonjour";
+        } else if(l.equals("italian")) {
+            greeting = "Bonjourno";
+        }else if(l.equals("spanish")) {
+            greeting = "Hola";
+        }else if(l.equals("german")) {
+            greeting = "Hallo";
+        }
+        return greeting + " " + n;
+    }
+
+    @GetMapping("form")
+    @ResponseBody
     public String helloForm(){
         return "<html>" +
                 "<body>" +
                 "<form action='hello' method='post'>" +//tells the form to submit the request to /hello
                 "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'" +
+                "<select name='language' id='language'>" +
+
+                "<option value='french'>French</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='italian'>Italian</option>" +
+                "<option value='english'>English</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
